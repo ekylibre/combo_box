fa8d118c290f68c8109b39305472f88c275ad924
Brice Texier <brice.texier@ekylibre.org>
2011-09-27 17:19:32 +0200

    Cleans JS and fixes a bug on method detection with Ruby 1.8.7

--- lib/combo_box/helpers/form_tag_helper.rb
+++ lib/combo_box/helpers/form_tag_helper.rb
@@ -26,16 +26,17 @@ module ComboBox
           choices = {:action=>action[1], :controller=>action[0]}
         end
         choices[:controller] ||= options.delete(:controller) || controller.controller_name
-        unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{choices[:action]}_in_#{choices[:controller]}".to_sym)
-          needed_controller = "#{choices[:controller].to_s.classify.pluralize}Controller"
-          needed_controller.constantize
-          unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{choices[:action]}_in_#{choices[:controller]}".to_sym)
-            raise Exception.new("It seems there is no search_for declaration corresponding to #{choices[:action]} in #{needed_controller}")
-          end
-        end
+        # unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{choices[:action]}_in_#{choices[:controller]}".to_sym)
+        #   needed_controller = "#{choices[:controller].to_s.classify.pluralize}Controller"
+        #   needed_controller.constantize
+        #   unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{choices[:action]}_in_#{choices[:controller]}".to_sym)
+        #     raise Exception.new("It seems there is no search_for declaration corresponding to #{choices[:action]} in #{needed_controller}")
+        #   end
+        # end
         html_options[:id] ||= "#{object_name}_#{method}"
         html  = ""
-        html << tag(:input, :type=>:text, "data-combo-box"=>url_for(choices.merge(:format=>:json)), "data-value-container"=>html_options[:id], :value=>ComboBox::CompiledLabels.send("item_label_for_#{choices[:action]}_in_#{choices[:controller]}", object.send(method.to_s.gsub(/_id$/,''))), :size=>html_options.delete(:size)||32)
+        # ComboBox::CompiledLabels.send("item_label_for_#{choices[:action]}_in_#{choices[:controller]}", object.send(method.to_s.gsub(/_id$/,'')))
+        html << tag(:input, :type=>:text, "data-combo-box"=>url_for(choices.merge(:format=>:json)), "data-value-container"=>html_options[:id], :value=>item_label(object.send(method.to_s.gsub(/_id$/,'')), choices[:action], choices[:controller]), :size=>html_options.delete(:size)||32)
         html << hidden_field(object_name, method, html_options)
         return html.html_safe
       end
@@ -79,14 +80,16 @@ module ComboBox
 
       def item_label(record, item_action, item_controller=nil)
         item_controller ||= controller.controller_name
-        unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{item_action}_in_#{item_controller}".to_sym)
+        method_name = "item_label_for_#{item_action}_in_#{item_controller}".to_sym
+        unless ComboBox::CompiledLabels.respond_to?(method_name)
           needed_controller = "#{item_controller.to_s.classify.pluralize}Controller"
           needed_controller.constantize
-          unless ComboBox::CompiledLabels.methods.include?("item_label_for_#{item_action}_in_#{item_controller}".to_sym)
-            raise Exception.new("It seems there is no search_for declaration corresponding to #{item_action} in #{needed_controller}")
+          # require "#{item_controller}_controller"
+          unless ComboBox::CompiledLabels.respond_to?(method_name)
+            raise Exception.new("It seems there is no 'search_for' declaration corresponding to #{item_action} in #{needed_controller} (#{ComboBox::CompiledLabels.methods.sort.to_sentence})")
           end
         end
-        return ComboBox::CompiledLabels.send("item_label_for_#{item_action}_in_#{item_controller}", record)
+        return ComboBox::CompiledLabels.send(method_name, record)
       end
 
     end
